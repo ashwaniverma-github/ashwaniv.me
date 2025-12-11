005a4d0f11bd9d72e0effae3d8314bafd6125a2b
ashwaniverma-github <ap8606574@gmail.com>
2025-12-11 07:36:31 +0530

    feedinbox

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -72,7 +72,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>{children}</body>
       <Script
-        src="http://feedinbox.com/widget.js"
+        src="https://www.feedinbox.com/widget.js"
         strategy="lazyOnload"
       />
       <Script id="feedinbox-config">
