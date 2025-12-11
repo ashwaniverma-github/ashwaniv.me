994137c248b0056c02bd2c5ac2d0b52780539929
ashwaniverma-github <ap8606574@gmail.com>
2025-12-11 07:49:35 +0530

    feedinbox3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,22 @@
 'use client'
 import CentreUpslider from "@/components/page";
+import Script from "next/script";
 
 export default function Home() {
   return (
     <div>
       <CentreUpslider/>
+      <Script
+        src="https://www.feedinbox.com/widget.js"
+        strategy="lazyOnload"
+      />
+      <Script id="feedinbox-config">
+        {`
+          window.feedinboxConfig = {
+            projectKey: "cmj0stzxh000cnruladng1ptx"
+          };
+        `}
+      </Script> 
     </div>
   );
 }
